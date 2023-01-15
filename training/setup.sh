5a2a0db20afd16a59784dde0a80551222f66ed58
Parker <parkumania@gmail.com>
2023-01-15 17:03:18 -0500

    update install script

--- training/setup.sh
+++ training/setup.sh
@@ -2,6 +2,9 @@
 # Creates a conda environment called capstone and installs the required packages we are using using conda package manager
 
 conda create -n capstone python=3.9
-conda install -n capstone -c conda-forge tensorflow ipykernel pyaudio librosa --update-deps --force-reinstall
+conda install -n capstone tensorflow
+pip install pyaudio
+pip install librosa
+pip install ipykernel
 
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CONDA_PREFIX/lib/
