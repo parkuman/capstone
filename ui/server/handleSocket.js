03ab07b55d88c15dccadd727393b1616a0066904
Parker Rowe <parker@prowe.ca>
2023-01-30 20:21:34 -0500

    websockets working

--- ui/server/handleSocket.js
+++ ui/server/handleSocket.js
@@ -1,7 +1,43 @@
+import { v4 as uuidv4 } from "uuid";
+import fs from "fs";
+import { Readable } from "stream";
+import { Blob } from "buffer";
+
 function handleSocket(io) {
 	io.on("connection", (socket) => {
-		socket.emit("server-connect-res", "yeah baby");
-		console.log("server emitted shit");
+		const newUUID = uuidv4();
+		console.log(`Client connected with uuid: ${newUUID}`);
+
+		const audio_chunks = [];
+		const writeStream = fs.createWriteStream("test.wav");
+
+		socket.on("begin_transcription", (audioSettings) => {
+			console.log(
+				"===============| begin_transcription |===============\n",
+				JSON.stringify(audioSettings)
+			);
+			socket.emit("ready_to_receive_chunks");
+		});
+
+		socket.on("audio_chunk", (chunk) => {
+			console.log("\nreceived audio chunk:\n", chunk);
+			audio_chunks.push(chunk);
+		});
+
+		socket.on("end_transcription", async () => {
+			console.log("===============| end_transcription |===============");
+			console.log("audio_chunks:\n", audio_chunks);
+
+			// https://stackoverflow.com/questions/60902935/how-to-save-mediarecorder-web-api-output-to-disk-using-a-stream
+			console.log("saving to file...");
+			const audio = new Blob(audio_chunks, { type: "audio/wav" });
+			const buffer = Buffer.from(await audio.arrayBuffer());
+			const readStream = Readable.from(buffer);
+
+			readStream.pipe(writeStream).on("finish", () => {
+				console.log("🎵 audio saved");
+			});
+		});
 	});
 }
 
--- ui/src/routes/+page.svelte
+++ ui/src/routes/+page.svelte
@@ -1,16 +1,55 @@
 <script lang="ts">
-  import { io, Socket } from "socket.io-client";
-  import { onMount } from "svelte";
+	import { io, Socket } from "socket.io-client";
+	import { onMount } from "svelte";
 
-  let socket: Socket;
+	let timeSlice = 1000; // ms
+	let socket: Socket;
+	let media = [];
+	let mediaRecorder = null;
+	let audioSettings;
 
-  onMount(() => {
-    socket = io();
-    socket.on("connect", () => {
-      console.log("client connect!");
-    });
-  });
+	async function getMicrophone() {
+		const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+		const audioTrack = stream.getAudioTracks()[0];
+		audioSettings = audioTrack.getSettings();
+
+		mediaRecorder = new MediaRecorder(stream);
+		mediaRecorder.ondataavailable = (e) => {
+      console.log(" - ondatavailable callback")
+			socket.emit("audio_chunk", e.data);
+		};
+
+		// mediaRecorder.onstop = function () {
+		// 	const audio = document.querySelector("audio");
+		// 	const blob = new Blob(media, { type: "audio/ogg; codecs=opus" });
+		// 	media = [];
+		// 	audio.src = window.URL.createObjectURL(blob);
+		// };
+	}
+
+	function beginTranscription() {
+		console.log("beginTranscription() - emitting begin_transcription with: ", audioSettings);
+		socket.emit("begin_transcription", audioSettings); // ask to send audio
+	}
+
+	function endTranscription() {
+		mediaRecorder.stop();
+    socket.emit("end_transcription");
+	}
+
+	onMount(async () => {
+		getMicrophone();
+		socket = io();
+		socket.on("connect", () => {
+			console.log("Successfully connected to server.");
+		});
+
+		socket.on("ready_to_receive_chunks", () => {
+			// start sending audio
+			mediaRecorder.start(timeSlice);
+		});
+	});
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<button on:click={beginTranscription}>Record</button>
+<button on:click={endTranscription}>Stop</button>
