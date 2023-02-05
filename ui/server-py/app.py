0f1b470a2c562a8a1da9e5134329093d8fef33c3
Parker Rowe <parker@prowe.ca>
2023-02-05 15:40:00 -0500

    whisper.cpp transcription

--- ui/server-py/app.py
+++ ui/server-py/app.py
@@ -1,4 +1,9 @@
 import tempfile
+import threading
+import subprocess
+
+import librosa    
+import soundfile as sf
 
 from flask import Flask
 from flask_socketio import SocketIO, send, emit
@@ -6,15 +11,58 @@ from flask_socketio import SocketIO, send, emit
 app = Flask(__name__)
 socketio = SocketIO(app, cors_allowed_origins="*")
 
+FILE_NAME = "audio"
 audio_bytes = b""
 is_first = True
 
 def save_blob_to_file(blob):
     fp = tempfile.NamedTemporaryFile()
-    print("\nTEMP FILE NAME" + fp.name + "\nFirst: " + str(is_first))
+    print("\nTEMP FILE NAME" + fp.name)
     fp.write(blob) 
     # fp.seek(0)
     # fp.close()
+
+def save_wav_16(bytes: bytes, file_name):
+    # write audio bytes to wav file
+    with open(file_name + ".wav", "wb") as file:
+      file.write(audio_bytes)
+
+    y, sr = librosa.load(file_name + ".wav", sr=16000) # Downsample 44.1kHz to 16kHz for whisper to be able to read it
+    sf.write(file_name + "_16.wav", y, sr, "PCM_16")   # save the 16KHz file as 16 bit (also for whisper)
+
+
+def popenAndCall(onExit, *popenArgs, **popenKWArgs):
+    """
+    Runs a subprocess.Popen, and then calls the function onExit when the
+    subprocess completes.
+
+    Use it exactly the way you'd normally use subprocess.Popen, except include a
+    callable to execute as the first argument. onExit is a callable object, and
+    *popenArgs and **popenKWArgs are simply passed up to subprocess.Popen.
+    """
+    def runInThread(onExit, popenArgs, popenKWArgs):
+        proc = subprocess.Popen(*popenArgs, **popenKWArgs)
+        proc.wait()
+        onExit()
+        return
+
+    thread = threading.Thread(target=runInThread,
+                              args=(onExit, popenArgs, popenKWArgs))
+    thread.start()
+
+    return thread # returns immediately after the thread starts
+
+
+def transcribe(file_name):
+    subprocess.run(["./main", "-f", "../capstone/ui/server-py/" + file_name + "_16.wav", 
+                    "--model", "models/ggml-large.bin", 
+                    "--output-txt"], 
+                    cwd="/home/parker/code/whisper.cpp")
+    
+    with open(file_name + "_16.wav.txt", "r") as f:
+        contents = f.readlines()
+        emit("finished_transcription", contents)
+
 
 @socketio.on("connect")
 def handle_message(data):
@@ -23,8 +71,10 @@ def handle_message(data):
 @socketio.on("begin_transcription")
 def handle_message(data):
     global is_first, audio_bytes
-    audio_bytes = b"" # clear the saved audio 
+
+    audio_bytes = b"" # clear the saved audio from the previous transcription
     is_first = True
+
     print("===============| begin_transcription |===============\n" + str(data))
     emit("ready_to_receive_chunks")
 
@@ -32,8 +82,7 @@ def handle_message(data):
 def handle_message(data):
     global is_first, audio_bytes
     
-    print("\nreceived audio chunk:\n", data[44:])
-    print("\n\n\n\nDATA HEADER:\n", data[:44])
+    # print("\nreceived audio chunk:\n", data)
     save_blob_to_file(data)
 
     audio_bytes += data if is_first else data[44:] # if its not the first chunk, get rid of WAV header (first 44 bytes)
@@ -43,8 +92,8 @@ def handle_message(data):
 @socketio.on("end_transcription")
 def handle_message():
     print("\===============| end_transcription |===============\n")
-    with open("audio.wav", 'wb') as file:
-      file.write(audio_bytes)
+    save_wav_16(audio_bytes, FILE_NAME)
+    transcribe(FILE_NAME)
 
 
 if __name__ == "__main__":
--- ui/src/routes/+page.svelte
+++ ui/src/routes/+page.svelte
@@ -5,53 +5,18 @@
 	import { MediaRecorder, register } from "extendable-media-recorder";
 	import { connect } from "extendable-media-recorder-wav-encoder";
 
+	import { wavHeader } from "$lib/utils";
+
 	let timeSlice = 1000; // ms
 	let socket: Socket;
 	let mediaRecorder: MediaRecorder;
 	let audioSettings: MediaTrackSettings;
 	let isRecording = false;
+	let awaitingTranscription = false;
 	let isFirstChunk = true;
-
-	/**
-	 * Creates a WAV header we can use to make each raw audio chunk understandable as
-	 * a WAV file.
-	 *
-	 * The first chunk of audio will already have this, but for subsequent ones
-	 * we need to add this ourselves
-	 *
-	 * See: https://github.com/chrisguttandin/extendable-media-recorder/issues/638
-	 * */
-	const wavHeader = (bitRate, numberOfChannels, numberOfSamples, sampleRate = Infinity) => {
-		const dataView = new DataView(new ArrayBuffer(44));
-
-		const bytesPerSample = bitRate >> 3; // tslint:disable-line:no-bitwise
-		/*
-		 * The maximum size of a RIFF file is 4294967295 bytes and since the header takes up 44 bytes there are 4294967251 bytes left for the
-		 * data chunk.
-		 */
-		const dataChunkSize = Math.min(numberOfSamples * numberOfChannels * bytesPerSample, 4294967251);
-
-		dataView.setUint32(0, 1380533830); // That's the integer representation of 'RIFF'.
-		dataView.setUint32(4, dataChunkSize + 36, true);
-		dataView.setUint32(8, 1463899717); // That's the integer representation of 'WAVE'.
-		dataView.setUint32(12, 1718449184); // That's the integer representation of 'fmt '.
-		dataView.setUint32(16, 16, true);
-		dataView.setUint16(20, 1, true);
-		dataView.setUint16(22, numberOfChannels, true);
-		dataView.setUint32(24, sampleRate, true);
-		dataView.setUint32(28, sampleRate * numberOfChannels * bytesPerSample, true);
-		dataView.setUint16(32, numberOfChannels * bytesPerSample, true);
-		dataView.setUint16(34, bitRate, true);
-		dataView.setUint32(36, 1684108385); // That's the integer representation of 'data'.
-		dataView.setUint32(40, dataChunkSize, true);
-
-		return dataView;
-	};
+	let finalTranscription = "";
 
 	async function getMicrophone() {
-		// register the wav encoder for extendable-media-recorder
-		await register(await connect());
-
 		const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
 		const audioTrack = stream.getAudioTracks()[0];
 		audioSettings = audioTrack.getSettings();
@@ -60,8 +25,8 @@
 		mediaRecorder = new MediaRecorder(stream, options);
 
 		mediaRecorder.ondataavailable = async (e) => {
+			// ensures we don't send extra audio after clicking stop
 			if (isRecording) {
-				// ensures we don't send extra audio after clicking stop
 				console.log("ondatavailable callback");
 
 				// prepend a wav header to each audio chunk (after the first one) so we can handle each as their own file
@@ -100,17 +65,24 @@
 		socket.emit("begin_transcription", audioSettings); // ask to send audio
 		isRecording = true;
 		isFirstChunk = true;
+		finalTranscription = "";
 	}
 
 	function endTranscription() {
 		isRecording = false;
 		mediaRecorder.stop();
 		socket.emit("end_transcription");
+		awaitingTranscription = true;
 	}
 
 	onMount(async () => {
+		// register the wav encoder for extendable-media-recorder
+		await register(await connect());
+
 		getMicrophone();
+
 		socket = io("http://localhost:5000/");
+		
 		socket.on("connect", () => {
 			console.log("Successfully connected to server.");
 		});
@@ -119,8 +91,20 @@
 			// start sending audio
 			mediaRecorder.start(timeSlice);
 		});
+
+		socket.on("finished_transcription", (data) => {
+			console.log("finished_transcription", data);
+			finalTranscription = data.join("<br />");
+			awaitingTranscription = false;
+		});
 	});
 </script>
 
-<button on:click={beginTranscription} disabled={isRecording}>Record</button>
-<button on:click={endTranscription} disabled={!isRecording}>Stop</button>
+<button on:click={beginTranscription} disabled={isRecording || awaitingTranscription}>Record</button>
+<button on:click={endTranscription} disabled={!isRecording || awaitingTranscription}>Stop</button>
+
+<h1>Transcription</h1>
+{#if awaitingTranscription}
+	<p>transcribing...</p>
+{/if}
+<p>{@html finalTranscription}</p>
